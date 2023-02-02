49bcc8d2388eb4db3037ab9ffccffd5041b9d80c
Anand <anandjha27122@gmail.com>
2023-02-02 12:42:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState("");
+  const [results, setResults] = useState([]);
+  
+  const fetchImages=()=>{
+    fetch(`https://api.unsplash.com/search/photos?client_id=SOm4KzsQxqXrbSppQ9o9RW7CSCbAoMBc2lWDnyhdt2I&query=${value}&orientation=squarish`)
+    .then(res=>res.json())
+    .then(data=>{
+      setResults(data.results)
+    })
+  }
+  return (
+    <div className="App">
+      <div className="mydiv">
+        <span>Search</span>
+        <input style={{width:"60%"}} type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+        <button onClick={()=>fetchImages()}>send</button>
+      </div>
+      <div className="gallery">  
+        {
+          results.map((item)=>{
+            return <img className="item" key={item.id} src={item.urls.regular} alt=""/>
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
